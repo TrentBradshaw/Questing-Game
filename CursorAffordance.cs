f098e306a47e011cf1262954d5edb9278ded4b91
TrentBradshaw <44883244+TrentBradshaw@users.noreply.github.com>
2019-01-26 03:09:17 -0800

    Add files via upload

---
+++ CursorAffordance.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CameraRaycaster))]
+public class CursorAffordance : MonoBehaviour {
+
+
+    [SerializeField] Texture2D walkCursor = null;
+    [SerializeField] Texture2D attackCursor = null;
+    [SerializeField] Texture2D questionCursor = null;
+    [SerializeField] Vector2 cursorHotspot = new Vector2(0, 0);
+
+    CameraRaycaster cameraRaycaster;
+	// Use this for initialization
+	void Start () {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+        cameraRaycaster.layerChangeObservers += OnLayerChanged;
+	}
+	
+	// Update is called once per frame
+	void OnLayerChanged (Layer newLayer) {
+        print("new layer");
+        switch (newLayer)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(questionCursor, cursorHotspot, CursorMode.Auto);  // So not set in default case
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(attackCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            default:
+                Debug.LogError("don't know what cursor to show");
+                return;
+        }
+     }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,61 @@
+
+
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float walkMoveStopRadius = 0.2f;
+
+    ThirdPersonCharacter thirdPersonCharacter;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+
+    
+
+    private void Start()
+    {
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
+        currentClickTarget = transform.position;
+    }
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        ProcessMouseMovement();
+    }
+
+  
+
+    private void ProcessMouseMovement()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            switch (cameraRaycaster.currentLayerHit)
+            {
+                case Layer.Walkable:
+                    currentClickTarget = cameraRaycaster.hit.point;
+                    break;
+                case Layer.Enemy:
+                    print("Not moving to enemy");
+                    break;
+                default:
+                    print("Unexpected layer found");
+                    return;
+            }
+        }
+
+        var playerToClickPoint = currentClickTarget - transform.position;
+        if (playerToClickPoint.magnitude >= walkMoveStopRadius)
+        {
+            thirdPersonCharacter.Move(playerToClickPoint, false, false);
+        }
+        else
+        {
+            thirdPersonCharacter.Move(Vector3.zero, false, false);
+        }
+    }
+}
