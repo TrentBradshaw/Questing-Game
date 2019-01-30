f4432f23eceb046d105a89a6acd4adfebed3337f
TrentBradshaw <44883244+TrentBradshaw@users.noreply.github.com>
2019-01-29 23:53:28 -0800

    Add files via upload
    refactored changes with a new setup

---
+++ Scripts/Inventory/InventoryUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public Transform itemsParent;  //get the itemsParent
+    public GameObject inventoryUI;
+
+    Inventory inventory;  // reference the player's inventory
+    InventorySlot[] slots; //create an array named slots
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = Inventory.instance;  //inventory is set as the instance of our inventory
+        inventory.onItemChangedCallback += UpdateUI;  //load UpdateUI to the delegate's list
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();  //slots is set to the items in the itemsParent list which is an array of all of the item slots
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Inventory"))
+        {
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+        }
+    }
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)  
+        {
+            if(i < inventory.items.Count)
+            {
+                slots[i].AddItem(inventory.items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
+    }
+}
---
+++ Scripts/Items/Item.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Item", menuName ="Inventory/Item")]
+public class Item : ScriptableObject
+{
+    new public string name = "New Item";    // Init the name of the item
+    public Sprite icon = null;              //Init the Icon sprite
+    public bool isDefaultItem = false;      // Init whether item is a default one or not
+    
+    public virtual void Use ()
+    {
+        Debug.Log("Using " + name);
+    }
+}
